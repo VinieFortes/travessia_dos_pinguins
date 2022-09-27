a921738cfbf4e6006f0ad0a028120492b9f10317
vinieFortes <vinie4apps@gmail.com>
2022-09-27 15:49:26 -0300

    hello wolrd

--- src/components/Winner.vue
+++ src/components/Winner.vue
@@ -2,7 +2,7 @@
   <q-dialog ref="dialog" persistent v-model="showDialog">
     <q-card style="background-color: #2a5f83; border: 1px solid white" class="text-white flex justify-center column items-center">
       <q-card-section class="q-pa-sm">
-        <span style="font-size: 22px; font-weight: bold">Parabéns você venceu!</span>
+        <span style="font-size: 22px; font-weight: bold">Parabéns Você Venceu!</span>
       </q-card-section>
       <q-card-section>
         <q-btn  @click="replayGame" color="white" text-color="blue-8" icon="replay" label="Tentar Novamente"></q-btn>
